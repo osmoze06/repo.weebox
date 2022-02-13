2afaf91c21461e020860c4fbcb7cb91b0833f6e2
osmoze06 <33359510+osmoze06@users.noreply.github.com>
2022-02-13 17:53:04 +0100

    Update upload_vstream_db_quit.py

--- repo/plugin.program.weebox/resources/lib/upload_vstream_db_quit.py
+++ repo/plugin.program.weebox/resources/lib/upload_vstream_db_quit.py
@@ -9,10 +9,6 @@ pDialog.create('WEEBOX', 'UPLOAD DATABASE, Envoi de la Bibliothèque et historiq
 xbmc.sleep(2000)
 pDialog.update(50, 'Upload de la DATABASE en cours...')
 
-source_dir1 = xbmcvfs.translatePath('special://home/addons/plugin.video.sendtokodiU2P/medias.bd')
-destination_dir1 = r"smb://KODI:Ongran2021@192.168.1.1/kodi/sync/medias.bd"
-xbmcvfs.copy(source_dir1, destination_dir1)
-
 source_dir2 = xbmcvfs.translatePath('special://home/userdata/addon_data/plugin.video.sendtokodiU2P/bookmark.db')
 destination_dir2 = r"smb://KODI:Ongran2021@192.168.1.1/kodi/sync/bookmark.db"
 xbmcvfs.copy(source_dir2, destination_dir2)
