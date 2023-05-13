7cbe6ae0cab093bc986244bc829261760c4a1e05
osmoze06 <33359510+osmoze06@users.noreply.github.com>
2023-05-13 17:36:43 +0200

    UPDATES

--- repo/plugin.program.weebox/resources/lib/skin_save_02.py
+++ repo/plugin.program.weebox/resources/lib/skin_save_02.py
@@ -17,8 +17,8 @@ destination_dir1 = xbmcvfs.translatePath('special://home/downloads/skin_save/01/
 source_dir2 = xbmcvfs.translatePath('special://home/userdata/guisettings.xml')
 destination_dir2 = xbmcvfs.translatePath('special://home/downloads/skin_save/01/guisettings.xml')
 
-source_dir3 = xbmcvfs.translatePath('special://home/addons/skin.arctic.horizon.2/extras/icons')
-destination_dir3 = xbmcvfs.translatePath('special://home/downloads/skin_save/01/addons/skin.arctic.horizon.2/extras/icons')
+#source_dir3 = xbmcvfs.translatePath('special://home/addons/skin.arctic.horizon.2/extras/icons')
+#destination_dir3 = xbmcvfs.translatePath('special://home/downloads/skin_save/01/addons/skin.arctic.horizon.2/extras/icons')
 
 source_dir4 = xbmcvfs.translatePath('special://home/addons/skin.arctic.horizon.2/1080i')
 destination_dir4 = xbmcvfs.translatePath('special://home/downloads/skin_save/01/addons/skin.arctic.horizon.2/1080i')
@@ -29,7 +29,7 @@ destination_dir5 = xbmcvfs.translatePath('special://home/downloads/skin_save/01/
 
 shutil.copytree(source_dir, destination_dir, dirs_exist_ok=True)
 shutil.copytree(source_dir1, destination_dir1, dirs_exist_ok=True)
-shutil.copytree(source_dir3, destination_dir3, dirs_exist_ok=True)
+#shutil.copytree(source_dir3, destination_dir3, dirs_exist_ok=True)
 shutil.copytree(source_dir4, destination_dir4, dirs_exist_ok=True)
 shutil.copytree(source_dir5, destination_dir5, dirs_exist_ok=True)
 shutil.copy(source_dir2, destination_dir2)
