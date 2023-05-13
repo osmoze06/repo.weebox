b2a215b9df42e8b425e0350c583f2841a7bff711
osmoze06 <33359510+osmoze06@users.noreply.github.com>
2023-05-13 13:16:27 +0200

    updates

--- repo/plugin.program.weebox/resources/lib/skin_save_02.py
+++ repo/plugin.program.weebox/resources/lib/skin_save_02.py
@@ -2,37 +2,41 @@
 # creation initiale par osmoze06 le 09 10 21
 import shutil
 import xbmc
+import xbmcvfs
 import os, sys, stat
-
-
-#Supression lecture seule du fichier
-#os.chmod(xbmc.translatePath('special://home/userdata/addon_data/plugin.program.weebox/skin_save/01/addon_data/skin.arctic.horizon.2/settings.xml'), 0o755)
 
 xbmc.executebuiltin("Notification(PREPARATION DES FICHIERS,Copie en cours...)")
 
 # COPIE DES DOSSIERS ET FICHIERS DU SKIN
-source_dir = xbmc.translatePath('special://home/userdata/addon_data/skin.arctic.horizon.2')
-destination_dir = xbmc.translatePath('special://home/userdata/addon_data/plugin.program.weebox/skin_save/01/addon_data/skin.arctic.horizon.2')
+source_dir = xbmcvfs.translatePath('special://home/userdata/addon_data/skin.arctic.horizon.2')
+destination_dir = xbmcvfs.translatePath('special://home/downloads/skin_save/01/addon_data/skin.arctic.horizon.2')
 
-source_dir1 = xbmc.translatePath('special://home/userdata/addon_data/script.skinshortcuts')
-destination_dir1 = xbmc.translatePath('special://home/userdata/addon_data/plugin.program.weebox/skin_save/01/addon_data/script.skinshortcuts')
+source_dir1 = xbmcvfs.translatePath('special://home/userdata/addon_data/script.skinshortcuts')
+destination_dir1 = xbmcvfs.translatePath('special://home/downloads/skin_save/01/addon_data/script.skinshortcuts')
 
-#source_dir2 = xbmc.translatePath('special://home/addons/skin.cosmic/1080i/script-skinshortcuts-includes.xml')
-#destination_dir2 = xbmc.translatePath('special://home/userdata/addon_data/plugin.program.weebox/skin_save/01/addons/skin.cosmic/1080i/script-skinshortcuts-includes.xml')
+source_dir2 = xbmcvfs.translatePath('special://home/userdata/guisettings.xml')
+destination_dir2 = xbmcvfs.translatePath('special://home/downloads/skin_save/01/guisettings.xml')
 
-source_dir3 = xbmc.translatePath('special://home/addons/skin.arctic.horizon.2/extras/icons')
-destination_dir3 = xbmc.translatePath('special://home/userdata/addon_data/plugin.program.weebox/skin_save/01/addons/skin.arctic.horizon.2/extras/icons')
+source_dir3 = xbmcvfs.translatePath('special://home/addons/skin.arctic.horizon.2/extras/icons')
+destination_dir3 = xbmcvfs.translatePath('special://home/downloads/skin_save/01/addons/skin.arctic.horizon.2/extras/icons')
+
+source_dir4 = xbmcvfs.translatePath('special://home/addons/skin.arctic.horizon.2/1080i')
+destination_dir4 = xbmcvfs.translatePath('special://home/downloads/skin_save/01/addons/skin.arctic.horizon.2/1080i')
+
+source_dir5 = xbmcvfs.translatePath('special://home/addons/skin.arctic.horizon.2/fonts')
+destination_dir5 = xbmcvfs.translatePath('special://home/downloads/skin_save/01/addons/skin.arctic.horizon.2/fonts')
+
 
 shutil.copytree(source_dir, destination_dir, dirs_exist_ok=True)
 shutil.copytree(source_dir1, destination_dir1, dirs_exist_ok=True)
 shutil.copytree(source_dir3, destination_dir3, dirs_exist_ok=True)
-#shutil.copy(source_dir2, destination_dir2)
+shutil.copytree(source_dir4, destination_dir4, dirs_exist_ok=True)
+shutil.copytree(source_dir5, destination_dir5, dirs_exist_ok=True)
+shutil.copy(source_dir2, destination_dir2)
 
-#Paramètre lecture seule du fichier
-#os.chmod(xbmc.translatePath('special://home/userdata/addon_data/plugin.program.weebox/skin_save/01/addon_data/skin.arctic.horizon.2/settings.xml'), 0o444)
 
 # CREATION ARCHIVE ZIP
-shutil.make_archive((xbmc.translatePath('special://home/userdata/addon_data/plugin.program.weebox/skin_save/skin.arctic.horizon.2')),'zip',(xbmc.translatePath('special://home/userdata/addon_data/plugin.program.weebox/skin_save/01')))
+shutil.make_archive((xbmcvfs.translatePath('special://home/downloads/skin_save/skin.arctic.horizon.2')),'zip',(xbmcvfs.translatePath('special://home/downloads/skin_save/01')))
 
-xbmc.executebuiltin("Notification(SKIN SAUVEGARDE, ARCTIC HORIZON 2 - Archive ZIP créée !)")
+xbmc.executebuiltin("Notification(SKIN SAUVEGARDE, Archive ZIP créée !)")
 sys.exit()
