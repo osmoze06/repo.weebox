2f213f408024aa9e9286b34c7bcef153810333b2
osmoze06 <33359510+osmoze06@users.noreply.github.com>
2023-07-02 19:13:49 +0200

    updates

--- repo/plugin.program.weebox/resources/lib/log_updates.py
+++ repo/plugin.program.weebox/resources/lib/log_updates.py
@@ -2,9 +2,16 @@ import xbmcgui
 import os
 import xbmcvfs
 import sys
+import urllib.request
+
+
+# Téléchargement du fichier texte
+sources_download = 'http://localhost/libreelec/clients/log_updates.txt'											
+sources_loc = xbmcvfs.translatePath('special://home/downloads/log_updates.txt')											
+urllib.request.urlretrieve(sources_download, sources_loc)
 
 # Chemin vers le fichier texte
-path = xbmcvfs.translatePath('special://home/addons/plugin.program.weebox/resources/lib/log_updates.txt')
+path = xbmcvfs.translatePath('special://home/downloads/log_updates.txt')
 
 # Lecture du contenu du fichier avec l'encodage utf-8
 with open(path, 'r', encoding='utf-8') as f:
--- repo/plugin.program.weebox/resources/lib/weebox_news.py
+++ repo/plugin.program.weebox/resources/lib/weebox_news.py
@@ -2,9 +2,16 @@ import xbmcgui
 import os
 import xbmcvfs
 import sys
+import urllib.request
+
+
+# Téléchargement du fichier texte
+sources_download = 'http://localhost/libreelec/clients/weebox_news.txt'											
+sources_loc = xbmcvfs.translatePath('special://home/downloads/weebox_news.txt')											
+urllib.request.urlretrieve(sources_download, sources_loc)
 
 # Chemin vers le fichier texte
-path = xbmcvfs.translatePath('special://home/addons/plugin.program.weebox/resources/lib/weebox_news.txt')
+path = xbmcvfs.translatePath('special://home/downloads/weebox_news.txt')
 
 # Lecture du contenu du fichier avec l'encodage utf-8
 with open(path, 'r', encoding='utf-8') as f:
