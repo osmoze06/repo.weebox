fc313c74c0ebbc2a0833a121229fcea4202c29c7
osmoze06 <33359510+osmoze06@users.noreply.github.com>
2023-09-16 13:17:42 +0200

    Update script_verif_kodi.py

--- repo/plugin.program.weebox/resources/lib/script_verif_kodi.py
+++ repo/plugin.program.weebox/resources/lib/script_verif_kodi.py
@@ -5,13 +5,13 @@ import xbmcvfs
 import sys
 
 kodi_version = xbmc.getInfoLabel("System.BuildVersion")
-latest_version = "20.1"
+latest_version = "20.2"
 
 if kodi_version < latest_version:
     message = "Votre version de Kodi : " + kodi_version +"\nVoulez-vous télécharger la dernière version de Kodi ?"
     choice = xbmcgui.Dialog().yesno("Info Version Kodi", message)
     if choice:
-        url = "https://ftp2.nluug.nl/mediaplayer/xbmc/releases/android/arm/kodi-20.1-Nexus-armeabi-v7a.apk"
+        url = "https://kodi.mirror.garr.it/releases/android/arm/kodi-20.2-Nexus-armeabi-v7a.apk"
         file_name = "/storage/emulated/0/Download/KODI.apk"
         progress = xbmcgui.DialogProgress()
         progress.create("Téléchargement", "Téléchargement en cours...")
@@ -26,10 +26,10 @@ if kodi_version < latest_version:
         if choice_run:
             xbmc.executebuiltin('StartAndroidActivity(com.lonelycatgames.Xplore)')
     else:
-        xbmcgui.Dialog().notification("Téléchargement", "Le téléchargement de la dernière version de Kodi a été annulé", xbmcgui.NOTIFICATION_WARNING, 5000)
+        xbmcgui.Dialog().notification("Téléchargement", "Le téléchargement de la dernière versaion de Kodi a été annulé", xbmcgui.NOTIFICATION_WARNING, 5000)
 
 else:
-    message = "Votre version de Kodi : " + kodi_version + "\nVous utilisez la dernière version de Kodi"
+    message = "Votre version de Kodi : " + kodi_version + "\nVous utilisez déjà la dernière version de Kodi."
     xbmcgui.Dialog().notification("Info Version Kodi", message, xbmcgui.NOTIFICATION_INFO, 5000)
 
 sys.exit()
