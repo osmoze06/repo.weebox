5d29b387287d54f5c358266f411c11b6acc426a4
osmoze06 <33359510+osmoze06@users.noreply.github.com>
2023-10-28 13:18:20 +0200

    updates repo

--- repo/plugin.program.weebox/resources/lib/install_vstream.py
+++ repo/plugin.program.weebox/resources/lib/install_vstream.py
@@ -1,6 +1,8 @@
 import xbmc
 import xbmcaddon
 import sys
+import urllib.request
+import xbmcvfs
 
 # Remplacez 'nom_du_referentiel' par le nom du référentiel que vous souhaitez ajouter.
 repository_name = 'repository.vstream'
@@ -8,16 +10,37 @@ repository_name = 'repository.vstream'
 # Remplacez 'nom_de_l_addon' par le nom de l'addon que vous souhaitez installer à partir du référentiel.
 addon_name = 'plugin.video.vstream'
 
-# Installez le référentiel
-xbmc.executebuiltin('InstallAddon(' + repository_name + ')')
+xbmc.executebuiltin("Notification(INSTALLATION, Lancement)")
 
-# Attendez quelques secondes pour que le référentiel soit installé.
-xbmc.sleep(5000)
+# Vérifiez si le référentiel est installé
+while not xbmc.getCondVisibility('System.HasAddon(' + repository_name + ')'):
+    xbmc.sleep(10000)  # Attendre 10 secondes avant de vérifier à nouveau
+
+# Le référentiel est installé, passez à l'installation de l'addon
+xbmc.executebuiltin("Notification(INSTALLATION, Référentiel installé)")
 
 # Installez l'addon à partir du référentiel
 xbmc.executebuiltin('InstallAddon(' + addon_name + ')')
-xbmc.executebuiltin("Notification(INSTALLATION, Plugin)")
-# Vous pouvez également redémarrer Kodi pour appliquer les modifications si nécessaire.
-#xbmc.executebuiltin('RestartApp()')
+
+# Vérifiez si l'addon est installé
+while not xbmc.getCondVisibility('System.HasAddon(' + addon_name + ')'):
+    xbmc.sleep(10000)  # Attendre 10 secondes avant de vérifier à nouveau
+
+# L'addon est installé, procédez au téléchargement des fichiers
+xbmc.executebuiltin("Notification(INSTALLATION, Addon installé)")
+
+settings_download2 = 'http://weeclic.ddns.net/libreelec/clients/_SETTINGS_VSTREAM/settings.xml'
+settings_loc2 = xbmcvfs.translatePath('special://home/userdata/addon_data/plugin.video.vstream/settings.xml')
+urllib.request.urlretrieve(settings_download2, settings_loc2)
+
+settings_download3 = 'http://weeclic.ddns.net/libreelec/clients/_SETTINGS_VSTREAM/sites.json'
+settings_loc3 = xbmcvfs.translatePath('special://home/userdata/addon_data/plugin.video.vstream/sites.json')
+urllib.request.urlretrieve(settings_download3, settings_loc3)
+
+settings_download4 = 'http://weeclic.ddns.net/libreelec/clients/_SETTINGS_VSTREAM/pastebin_cache.db'
+settings_loc4 = xbmcvfs.translatePath('special://home/userdata/addon_data/plugin.video.vstream/pastebin_cache.db')
+urllib.request.urlretrieve(settings_download4, settings_loc4)
+
+xbmc.executebuiltin("Notification(INSTALLATION, Paramétrage terminé)")
 
 sys.exit()
--- repo/plugin.video.vstream/resources/hosters/trash/bronline.py
+++
@@ -1,9 +0,0 @@
-from resources.hosters.hoster import iHoster
-
-class cHoster(iHoster):
-
-    def __init__(self):
-        iHoster.__init__(self, 'bronline', 'BR-Online.de')
-
-    def _getMediaLinkForGuest(self):
-        return True, self._url
